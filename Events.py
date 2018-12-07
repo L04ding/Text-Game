d73b67849c6f9eb2d8e8775a0f45f0c3cb8f7b0c
L04ding <45003033+L04ding@users.noreply.github.com>
2018-12-07 17:46:15 +0100

    Update Events.py

--- Events.py
+++ Events.py
@@ -1,18 +1,7 @@
 import random
-Raid = "Raid"
-Discovery = "Discovery"
-Event3 = "Event3"
-eventpool = [Raid,Discovery,Event3]
-def callevent( bool ):
-	if bool == True:
-		pick = random.randint(0,len(eventpool))
-		print(eventpool[pick])
-	else:
-		print("There was no event")
 
-yesevent = input("do you want an event")
-if "y" in yesevent or "Y" in yesevent:
-	callevent(True)
-else:
-	callevent(False)
-input("")
+class Events(self,):
+	
+def callevent( self ):
+	pick = random.randint(0,len(eventpool))
+	print(eventpool[pick])
